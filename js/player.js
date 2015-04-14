bb5261fd5ce118a4b4d2bd687a44f930472a0f87
asriniva <asriniva@mit.edu>
2015-04-14 17:05:13 -0400

    added email

--- js/player.js
+++ js/player.js
@@ -2,7 +2,7 @@ angular.module('playerApp', [])
   .controller('PlayerController', function() {
     var playerList = this;
     playerList.players = [
-      {name:'Joe Joeson', checkmark:false, position:"stuff", rating:5, gpa:4.0, sat: 1600, film:"file://null"}];
+      {name:'Joe Joeson', email: "joe@gmail.com", checkmark:false, position:"stuff", rating:5, gpa:4.0, sat: 1600, film:"file://null"}];
 
     playerList.addPlayer = function(player) {
       playerList.players.push(player);
@@ -11,6 +11,7 @@ angular.module('playerApp', [])
       playerList.players.remove(player);
     };
 
+    // inspired by http://stackoverflow.com/questions/1129216/sorting-objects-in-an-array-by-a-field-value-in-javascript
     playerList.sortByAscending = function(property) {
       return function(a, b) {
         return (a[property] > b[property]) ? 1 : (a[property] < b[property]) ? -1 : 0;
