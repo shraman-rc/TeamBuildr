0d211991e28a6806ccb2ba4cca151d3caab6c8a4
asriniva <asriniva@mit.edu>
2015-04-14 16:49:21 -0400

    added film

--- js/player.js
+++ js/player.js
@@ -2,7 +2,7 @@ angular.module('playerApp', [])
   .controller('PlayerController', function() {
     var playerList = this;
     playerList.players = [
-      {name:'Joe Joeson', checkmark:false, position:"stuff", rating:5, gpa:4.0, sat: 1600}];
+      {name:'Joe Joeson', checkmark:false, position:"stuff", rating:5, gpa:4.0, sat: 1600, film:"file://null"}];
 
     playerList.addPlayer = function(player) {
       playerList.players.push(player);
